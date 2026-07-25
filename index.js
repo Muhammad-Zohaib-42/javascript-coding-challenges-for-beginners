fc5627709e2f33a1e711dfa49c53f49104a163e9
Zohaib <ranjha5054@gmail.com>
2026-07-25 13:32:02 +0500

    test for calculating milliseconds is done

--- index.js
+++ index.js
@@ -21,3 +21,40 @@ export function multiplesOf3Or5(number) {
 
     return multiplesOf3And5.reduce((acc, curr) => acc + curr, 0)
 }
+
+
+/* 
+
+3. Clock
+
+The clock shows h hours (0 <= h <= 23), m minutes (0 <= m <= 59) and s seconds (0 <= s <= 59) after midnight. Your task is to write a function which returns the time since midnight in milliseconds. There are 1,000 milliseconds in a second.
+
+*/
+
+export function past(h, m, s) {
+    let milliseconds = 0
+
+    if (h > 0) milliseconds = h * 60
+
+    if (m > 0) {
+        milliseconds = milliseconds > 0 ? (milliseconds + m) * 60 : m * 60
+    } else {
+        milliseconds = milliseconds > 0 ? milliseconds * 60 : 0
+    }
+
+    if (s > 0) {
+        milliseconds = milliseconds > 0 ? (milliseconds + s) * 1000 : s * 1000
+    } else {
+        milliseconds = milliseconds > 0 ? milliseconds * 1000 : 0
+    }
+
+    return milliseconds
+}
+
+/*
+
+5. Century From Year
+
+The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc. Given a year, return the century it is in.
+
+*/
--- index.test.js
+++ index.test.js
@@ -1,8 +1,12 @@
 import {describe, it, expect} from "vitest"
-import { multiplesOf3Or5 } from "."
+import { multiplesOf3Or5, past } from "."
 
-describe("sum of all multiples of 3 or 5", () => {
-    it ("this function returns sum of multiples of 3 or 5", () => {
+/*
+
+1. sum of multiples of 3 or 5
+
+describe("multiplesOf3Or5", () => {
+    it ("should return sum of multiples of 3 or 5", () => {
         expect(multiplesOf3Or5(0)).toBe(0)
         expect(multiplesOf3Or5(-15)).toBe(0)
         expect(multiplesOf3Or5(10)).toBe(23)
@@ -10,3 +14,22 @@ describe("sum of all multiples of 3 or 5", () => {
         expect(multiplesOf3Or5(200)).toBe(9168)
     })
 })
+
+*/
+
+
+/*
+
+2. calculate the milliseconds
+
+describe("past", () => {
+    it("should returns the time since midnight in milliseconds", () => {
+        expect(past(0, 0, 0)).toBe(0)
+        expect(past(0, 1, 1)).toBe(61000)
+        expect(past(1, 0, 0)).toBe(3600000)
+        expect(past(1, 0, 1)).toBe(3601000)
+        expect(past(1, 1, 1)).toBe(3661000)
+    })
+})
+
+*/
